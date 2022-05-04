6507c4e5eea4c86a8d276d2e77cc3a81c20e4ea7
harisbegic <haris.begic101@gmail.com>
2022-05-04 13:00:40 +0200

    Add more checks and bug fix

--- index.js
+++ index.js
@@ -1,51 +1,94 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Browser } = require('selenium-webdriver');
+const { withTagName } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { titleContains } = require('selenium-webdriver/lib/until');
 const expect = require('chai').expect;
 
-(async function firstScript() {
+(async function smokeTest() {
   try {
+    // Chrome Builder
     let driver = await new Builder().forBrowser('chrome').build();
 
+    // GET functions
+    getElementById = async (id) => await driver.findElement(By.id(id));
+    getElementByClass = async (className) => await driver.findElement(By.className(className));
+    getElementByXpath = async (xpath) => await driver.findElement(By.xpath(xpath));
+    
+    // Initialize website
     await driver.get('https://www.navigator.ba');
 
-    let search = await driver.findElement(By.id('ember564'));
+    // Search bar testing whether the results match typed word
+    let search = await getElementById('ember564');
 
     await search.click();
-    await search.sendKeys('Pekara');
+    await search.sendKeys('Pekara')
+    await driver.wait(until.elementLocated(By.className('tt-suggestion'), 10000));
 
-    let choices = await driver.findElements(By.className('tt-sugestion'));
+    let choicesElement = await getElementByClass('tt-suggestions');
+    let choices = await choicesElement.findElements(By.className('tt-suggestion'));
+
     for (const choice of choices) {
-        expect(choice.getText()).toContain('Pekara');
+      expect(await choice.getText()).to.include('Pekara');
     }
 
-    let zoomIn = await driver.findElement(By.className('leaflet-control-zoom-in'));
+    // slow it down :)
+    await driver.sleep(2000);
+
+    // zooming functionality
+    let zoomIn = await getElementByClass('leaflet-control-zoom-in');
     await zoomIn.click();
 
-    let zoomOut = await driver.findElement(By.className('leaflet-control-zoom-out'));
+    let zoomOut = await getElementByClass('leaflet-control-zoom-out');
     await zoomOut.click();
 
-    let enterCategory = await driver.findElement(By.id('ember713'));
+    // entering into one of the categories and testing whether the object address matches address on map
+    let enterCategory = await getElementById('ember713');
     await enterCategory.click();
 
-    let enterObject = await driver.findElement(By.id('ember1459'));
+    await driver.sleep(3000);
+
+    await driver.wait(until.elementLocated(By.className('menu_content_list list-items'), 10000));
+
+    let objectsElement = await getElementByClass('ember-view left-menu-pane list-container mCustomScrollbar _mCS_3');
+    let enterObject = await objectsElement.findElement(By.className('menu_content_list list-items'));
+    
     await enterObject.click();
+    await driver.sleep(3000);
+    await driver.wait(until.elementLocated(By.className('show-on-map'), 10000));
 
-    let showOnMap = await driver.findElement(By.className('show-on-map'));
-    await showOnMap.click();
+    let address = await getElementByXpath('//*[@id="mCSB_4"]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/span');
+    let mapAddress = await getElementByXpath('//*[@id="ember628"]/div[2]/div[2]/div[4]/div/div[1]/div/div/div[1]/div[2]')
 
-    let clickFeedback = await driver.findElement(By.id('ember587'));
+    expect(await address.getText()).to.be.equal(await mapAddress.getAttribute('title')); 
+
+    // enter feedback page
+    let clickFeedback = await getElementById('ember587');
     await clickFeedback.click();
 
-    let clickCreatePlace = await driver.findElement(By.id('ember587'));
+    await driver.sleep(2000);
+
+    // enter create a place page
+    let clickCreatePlace = await getElementById('ember572');
     await clickCreatePlace.click();
 
-    let translateToBosnian = await driver.findElement(By.className('btn-bs'));
-    await translateToBosnian.click();
-
-    let translateToEnglish = await driver.findElement(By.className('btn-bs'));
+    // test translating to English
+    let translateToEnglish = await getElementByClass('btn-en');
     await translateToEnglish.click();
 
+    let englishCheck = await getElementById('ember586');
+    expect(await englishCheck.getAttribute('placeholder')).to.include('Search street or place');
+
+    // test translating back to Bosnian
+    let translateToBosnian = await getElementByClass('btn-bs');
+    await translateToBosnian.click();
+
+    let bosnianCheck = await getElementById('ember586');
+    expect(await bosnianCheck.getAttribute('placeholder')).to.include('Traži ulicu ili objekat');
+
+    await driver.sleep(3000);
+
     await driver.quit();
-        
+
   } catch (error) {
     console.log(error)
   }
